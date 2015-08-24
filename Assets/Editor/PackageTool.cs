7ef558c4cae0be2c53d4039168e740d8cf8a1a1c
Keijiro Takahashi <keijiro@ZX.local>
2015-08-24 22:04:07 +0900

    added unitypackage file

---
+++ Assets/Editor/PackageTool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PackageTool
+{
+    [MenuItem("Package/Update Package")]
+    static void UpdatePackage()
+    {
+        AssetDatabase.ExportPackage("Assets/Kvant", "KvantGrass.unitypackage", ExportPackageOptions.Recurse);
+    }
+}
